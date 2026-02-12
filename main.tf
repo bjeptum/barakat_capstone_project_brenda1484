74f0a1b24a40d8ea919362516cc45643f1dc44d7
b_jeptum <brendajeptum@gmail.com>
2026-02-12 04:19:31 +0300

    Default tagging structure added

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 provider "aws" {
   region = "us-east-1"
+   default_tags {
+    tags = {
+      Project         = "barakat-2025-capstone"
+      AdmissionNumber = "1484"
+    }
+  }
 }
 
 data "aws_caller_identity" "current" {}
