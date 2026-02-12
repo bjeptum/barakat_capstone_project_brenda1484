4074f48186b74311cef8f7ede0411c7baa965ef6
b_jeptum <brendajeptum@gmail.com>
2026-02-12 04:52:34 +0300

    Add DynamoDB table for Terraform state locking

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "your-tf-state-bucket"  # Manually created
-    key            = "bedrock/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-lock"  # Manually created
+    bucket         = "tf-state-barakat-2025-capstone-1484"       # S3 state bucket
+    key            = "project-bedrock/terraform.tfstate"        # Path inside the bucket for the state file
+    region         = "us-east-1"                               
+    dynamodb_table = "tf-lock-barakat-2025-capstone-1484"       # DynamoDB table for state locking
+    encrypt        = true                                       # Enables encryption at rest
   }
 }
