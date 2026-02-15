de69da14599459f9d1b3cb59261751e9648bba69
b_jeptum <brendajeptum@gmail.com>
2026-02-16 02:53:18 +0300

    Update .gitignore to exclude grading.json

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-barakat-2025-capstone-1484"       # S3 state bucket
+    bucket         = "tf-state-barakat-2025-capstone-1484"      # S3 state bucket
     key            = "project-bedrock/terraform.tfstate"        # Path inside the bucket for the state file
     region         = "us-east-1"                               
     dynamodb_table = "tf-lock-barakat-2025-capstone-1484"       # DynamoDB table for state locking
