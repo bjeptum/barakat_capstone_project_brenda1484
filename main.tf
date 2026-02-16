66a6e320139a629182ec1b9fd4e68ddbf4641677
b_jeptum <brendajeptum@gmail.com>
2026-02-16 15:53:46 +0300

    Add new Terraform resources and update configuration

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "eks" {
   version = "~> 20.0"
 
   cluster_name    = "barakat-2025-capstone-cluster"
-  cluster_version = "1.33"
+  cluster_version = "1.34"
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
